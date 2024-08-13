3c6817ec0e1ef82d0924bcb2c272948a69dacac4
Somin0211 <144300047+Somin0211@users.noreply.github.com>
2024-08-14 01:55:50 +0900

    Add files via upload
    동전 0 / 실버 4 / 18분

---
+++ 2주차/# 11047 동전 0.py
@@ -0,0 +1,16 @@
+# 11047 동전 0
+N, K = map(int, input().split())
+coins=[]
+for i in range(N):
+    coins.append(int(input()))
+coins.sort(reverse=True)
+
+result = 0
+for i in coins:
+    if K == 0: 
+      break
+    result += K // i 
+    K %= i 
+    print(K,i)
+
+print(result)
